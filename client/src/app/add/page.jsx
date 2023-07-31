101c14cf03bd6a5545d0e0e46d5d1b21ae130f01
Julie Chan <juliechan03@gmail.com>
2023-07-30 23:53:52 -0700

    added-use-client-to-files

--- client/src/app/add/page.jsx
+++ client/src/app/add/page.jsx
@@ -1,12 +1,37 @@
+'use client'
+
 import React, { useEffect, useState } from 'react';
 import Link from 'next-routes';
+import { useRouter } from 'next/navigation';
 import BusinessForm from '../components/BusinessForm';
 import LogoutButton from '../components/LogoutButton';
-import CancelButton from '../components/CancelButton';
 
 const AddBusiness = () => {
     const [ biz, setBiz ] = useState([]);
     const [ errors, setErrors ] = useState({});
+	const {push} = useRouter();
+
+
+    const createBiz = (e) => {
+        e.preventDefault;
+        fetch('http://localhost:8080/api/business', {
+            body: JSON.stringify(biz),
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            console.log(res);
+            if(res.status == 201){
+                push('/')
+            }
+        })
+        .catch(err => {
+            alert('Errors detected! Check console for message!');
+            console.log(err);
+        })
+    }
 
     return (
         <>
